bbbf47e0ecd152bd76bc9012e85c473b887bb27e
Ashmit Singh Gogia <ashmit9955@gmail.com>
2025-12-07 03:40:32 +0530

    feat: add questions table creation

--- main.go
+++ main.go
@@ -20,4 +20,21 @@ func main() {
 		log.Fatal("Can't connect to DB: ", err)
 	}
 	fmt.Println("Connected to CS_MasterDB successfully!")
+	createQuestionsTable(db)
+	fmt.Println("Questions table ready!")
+}
+
+func createQuestionsTable(db *sql.DB) {
+	query := `
+	CREATE TABLE IF NOT EXISTS questions(
+		id SERIAL PRIMARY KEY,
+		questions_text TEXT NOT NULL,
+		options TEXT NOT NULL,
+		correct_index INT NOT NULL
+	)
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal("Error creating table:, ", err)
+	}
 }
