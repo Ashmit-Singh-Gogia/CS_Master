3d89e4385681259f983523982bac14b75d6bf895
Ashmit Singh Gogia <ashmit9955@gmail.com>
2025-12-08 12:00:02 +0530

    fix: use chi v5 in handlers to enable URL params correctly

---
+++ internal/routes/router.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	handlers "CS_Master/internal/handlers"
+	"database/sql"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRouter(db *sql.DB) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Quiz API Running"))
+	})
+
+	// Question routes
+	r.Post("/questions", handlers.CreateQuestions(db))
+	r.Get("/questions", handlers.GetAllQuestions(db))
+	r.Get("/questions/{id}", handlers.GetOneQuestion(db))
+	r.Put("/questions/{id}", handlers.UpdateQuestion(db))
+	r.Delete("/questions/{id}", handlers.DeleteQuestion(db))
+	r.Post("/questions/{id}/check", handlers.CheckAnswer(db))
+
+	return r
+}
